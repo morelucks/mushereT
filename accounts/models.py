251729315fca7bb61a2171fa3ffed946019a4b57
morelucks <luckykamshak@gmail.com>
2023-10-17 20:31:04 +0000

    login template

--- accounts/models.py
+++ accounts/models.py
@@ -12,7 +12,7 @@ class User(AbstractUser):
     surname = models.CharField(max_length=30)
     mobile_number = models.CharField(max_length=15, blank=True, null=True)
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
-    new_password = models.CharField(max_length=128)
+   
 
     def __str__(self):
         return f'{self.first_name} {self.surname}'
